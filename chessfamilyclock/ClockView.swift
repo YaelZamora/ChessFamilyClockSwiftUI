e2358a5f63521b814fa000a478fb0e61d66d466d
YaelZamora <ajedrezkasparov@gmail.com>
2023-03-17 15:00:11 -0600

    first commit

---
+++ chessfamilyclock/ClockView.swift
@@ -0,0 +1,20 @@
+//
+//  ClockView.swift
+//  chessfamilyclock
+//
+//  Created by Yael Javier Zamora Moreno on 17/03/23.
+//
+
+import SwiftUI
+
+struct ClockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClockView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClockView()
+    }
+}
