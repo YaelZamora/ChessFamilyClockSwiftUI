8cee46ad1c83b7403311be3a689c401d8c9be1f9
YaelZamora <ajedrezkasparov@gmail.com>
2023-03-17 12:41:49 -0600

    Initial Commit

---
+++ chessfamilyclock/chessfamilyclockApp.swift
@@ -0,0 +1,17 @@
+//
+//  chessfamilyclockApp.swift
+//  chessfamilyclock
+//
+//  Created by Yael Javier Zamora Moreno on 17/03/23.
+//
+
+import SwiftUI
+
+@main
+struct chessfamilyclockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
